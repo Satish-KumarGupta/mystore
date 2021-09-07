385c4e06d4354c502f6a6d14c5d43f808a77bfed
satish122 <satishkumar998475@gmail.com>
2021-09-07 17:58:42 +0530

    navigation threw Link

---
+++ pages/text.js
@@ -0,0 +1,13 @@
+
+
+import Link from 'next/link'
+const  Text=() =>{
+    return (
+      <div>
+        <h1>welcome to Text  js</h1>
+        <Link href="/"><a>go back home</a></Link>
+      </div>
+    )
+  }
+  
+  export default Text;
