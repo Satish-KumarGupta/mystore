8db67bf73a0090283d16dc669ebd96ce83224710
satish122 <satishkumar998475@gmail.com>
2021-09-08 20:00:14 +0530

    fetch productdetail from getStaticProps using fallback false in path

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -7,12 +7,28 @@ const Product = ({product}) =>{
     )
 }
 
-export async function getServerSideProps({params:{id}}){
-   const res = await fetch(`http://localhost:3000/api/product/${id}`)
-   const data = await res.json()
-   return{
-        props: {product:data}
-    }
-}
+// export async function getServerSideProps({params:{id}}){
+//    const res = await fetch(`http://localhost:3000/api/product/${id}`)
+//    const data = await res.json()
+//    return{
+//         props: {product:data}
+//     }
+// }
 
+export async function getStaticProps({params:{id}}){
+    const res = await fetch(`http://localhost:3000/api/product/${id}`)
+    const data = await res.json()
+    return{
+         props: {product:data}
+     }
+ }
+ 
+ export async function getStaticPaths() {
+    return {
+      paths: [
+        { params: { id:"613885623c3c4c08cd8ed3fb"} } // See the "paths" section below
+      ],
+      fallback:  false
+    };
+  }
 export default Product
