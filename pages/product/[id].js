069a93d54c83d1eb20e995e59db2209c623f3141
satish122 <satishkumar998475@gmail.com>
2021-09-08 20:09:11 +0530

    fetch productdetail from getStaticProps using fallback true in path

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,5 +1,11 @@
-
+import {useRouter} from 'next/router'
 const Product = ({product}) =>{
+    const router = useRouter()
+    if(router.isFallback){
+        return (
+            <h3> loading.. </h3>
+        )
+    }
     return(
         <div>
             <h1> {product.name} </h1>
@@ -26,9 +32,9 @@ export async function getStaticProps({params:{id}}){
  export async function getStaticPaths() {
     return {
       paths: [
-        { params: { id:"613885623c3c4c08cd8ed3fb"} } // See the "paths" section below
+        { params: { id:"613885623c3c4c08cd8ed3fb"} } 
       ],
-      fallback:  false
+      fallback: true
     };
   }
 export default Product
