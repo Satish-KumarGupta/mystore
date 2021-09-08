845471e4c7f97d17333eae5557e6a35a228d1cb9
satish122 <satishkumar998475@gmail.com>
2021-09-08 21:03:07 +0530

    add model on click delete button

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,13 +1,41 @@
 import {useRouter} from 'next/router'
 import baseUrl from '../../Helper/baseUrl'
+import { useRef,useEffect,useState} from 'react'
 const Product = ({product}) =>{
     const router = useRouter()
+    const modalRef= useRef(null)
     if(router.isFallback){
         return (
             <h3> loading.. </h3>
         )
     }
-    return(
+
+    const getModal = ()=>{
+        return(
+         <div id="modal1" className="modal" ref={modalRef}>
+         <div className="modal-content">
+           <h4>{product.name}</h4>
+           <p>Are you sure you want to delete this</p>
+         </div>
+         <div className="modal-footer">
+         <button className="btn waves-effect waves-light #1565c0 blue darken-3">
+           cancel
+         </button>
+         <button className="btn waves-effect waves-light #c62828 red darken-3"
+         onClick={()=>deleteProduct()}
+        >
+           Yes
+         </button>
+         </div>
+       </div>
+               
+        )
+      }
+      useEffect(()=>{
+        M.Modal.init(modalRef.current)
+      },[])
+
+      return(
         <div className="container certer-align">
             <h3> {product.name} </h3>
             <img src={product.mediaUrl} style={{width: "30%"}}/>
@@ -27,7 +55,11 @@ const Product = ({product}) =>{
             </button>
             <p className="left-align">
                 {product.description}
-            </p>
+            </p> 
+            <button data-target="modal1" className="btn modal-trigger waves-effect waves-light #c62828 red darken-3">Delete
+              <i className="material-icons left">delete</i>
+            </button>
+            {getModal()}
         </div>
     )
 }
