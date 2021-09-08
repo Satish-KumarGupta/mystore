d6982ba1c682421b5c9b359d1b906017b2ef5234
satish122 <satishkumar998475@gmail.com>
2021-09-08 15:29:45 +0530

    fetch product from api

--- pages/api/product.js
+++ pages/api/product.js
@@ -1,9 +1,13 @@
 /* eslint-disable import/no-anonymous-default-export */
 // eslint-disable-next-line import/no-anonymous-default-export
 import initDB from "../../Helper/initDb"
+import Product from '../../models/Product'
 
 initDB()
-export default (_req ,res) => {
-    res.json({message:"hello world"})
+
+export default (req ,res) => {
+  Product.find().then(produts=>{
+    res.status(200).json(produts)
+  }) 
+
   }
-  
