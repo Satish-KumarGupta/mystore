11c9ef0e5333d52e6851b0c63339a18ea0a3e018
satish122 <satishkumar998475@gmail.com>
2021-09-09 15:32:07 +0530

    navbar page sets on login

--- Component/NavBar.js
+++ Component/NavBar.js
@@ -1,8 +1,17 @@
 import { useRouter } from "next/dist/client/router"
 import Link from "next/link"
+import {parseCookies} from 'nookies'
 const NavBar =()=> {
     
     const router= useRouter()
+    const {tokan}=parseCookies()
+    let user= false
+    if(tokan){
+      user=true
+    } 
+    else{
+      user=false
+    }
     function isActive(route){
         if(route===router.pathname){
             return "active"
@@ -15,9 +24,23 @@ const NavBar =()=> {
         <div className="nav-wrapper blue">
           <Link href="/"><a className="brand-logo">Logo</a></Link>
           <ul id="nav-mobile" className="right ">
+            {
+            user &&
+              <li className={isActive('/create')}><Link  href="/create"><a>create</a></Link></li>
+            }
+           
+           { user ?
+            <>
+            <li className={isActive('/account')}><Link  href="/account"><a>account</a></Link></li>
+            <li><button className="btn">Logout</button></li>
+            </>
+           :
+           <>
             <li className={isActive('/login')}><Link  href="/login"><a>login</a></Link></li>
             <li className={isActive('/signup')}><Link  href="/signup"><a>signup</a></Link></li>
-            <li className={isActive('/create')}><Link  href="/create"><a>create</a></Link></li>
+            {/* <li className={isActive('/create')}><Link  href="/create"><a>create</a></Link></li> */}
+           </>
+           }
           </ul>
         </div>
       </nav>
