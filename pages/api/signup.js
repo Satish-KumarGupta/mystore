cb01393441b09b204d0ca246a63682080d967ff6
satish122 <satishkumar998475@gmail.com>
2021-09-09 21:55:22 +0530

    create cart api

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
 import initDB from '../../Helper/initDB'
 import User from '../../Models/User'
 import bcrypt from 'bcryptjs'
--- pages/cart.js
+++ pages/cart.js
@@ -1,10 +1,27 @@
-
+import baseUrl from "../Helper/baseUrl"
+import { parseCookies } from "nookies"
 const cart = () => {
     return (
     <div>
         cart page
     </div>
-    )
+     )
+}
+
+export async function getServerSideProps(ctx){
+    const {token} =parseCookies(ctx)
+    const res =await fetch(`${baseUrl}/api/cart`,{
+        headers:{
+            // "Authorization":"bearer"+token   "bearer is optinal"
+            "Authorization":token
+
+        }
+    })
+    const products = await res.json()
+    console.log(products)
+    return{
+        props:{products}
+    }
 }
 
 export default cart
